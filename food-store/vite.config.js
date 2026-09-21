05f06697e39907ad7b815ee240b1e08f1e2992fc
Ced <conconcedric@gmail.com>
2026-09-21 19:22:58 +0800

    fix: vite config for deployment

--- food-store/vite.config.js
+++ food-store/vite.config.js
@@ -1,11 +1,13 @@
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],  
+  ],
+  css: {
+    transformer: 'postcss'
+  }
 })
