6117e9237f977abab8c9cc02b23cc40bd5a434bd
Ced <conconcedric@gmail.com>
2026-09-21 19:27:54 +0800

    fix: switch to tailwind v3 for deployment

--- food-store/vite.config.js
+++ food-store/vite.config.js
@@ -1,13 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  css: {
-    transformer: 'postcss'
-  }
+  plugins: [react()],
 })
